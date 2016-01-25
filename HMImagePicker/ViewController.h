a0aa2041256058db8951607d1e5fae9947949f7a
刀哥 <liufan321@gmail.com>
2016-01-25 08:18:10 +0800

    init project

---
+++ HMImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMImagePicker
+//
+//  Created by 刘凡 on 16/1/25.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HMImagePicker/ViewController.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  HMImagePicker
+//
+//  Created by 刘凡 on 16/1/25.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (IBAction)clickSelectPhotoButton {
+    NSLog(@"%s", __FUNCTION__);
+}
+
+@end
